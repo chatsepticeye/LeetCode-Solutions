fed6dcd32d10c13b3d6329f2c1ee2a01247f67d8
chatsepticeye <chatprat22@gmail.com>
2026-01-15 19:38:59 +0530

    Time: 10 ms (24.77%), Space: 49.2 MB (27.77%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) return intervals;
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        List<int[]>result= new ArrayList<>();
+        int [] current=intervals[0];
+        result.add(current);
+        for(int[] interval:intervals){
+            int currentend=current[1];
+            int nextstart=interval[0];
+            int nextend=interval[1];
+            if(currentend>=nextstart){
+                current[1]=Math.max(currentend,nextend);
+        }
+            else{
+                current=interval;
+                result.add(current);
+            }
+
+
+            
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+       
+        
+}
