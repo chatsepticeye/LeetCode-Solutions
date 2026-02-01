e9f9e1ef45083c6f5afb09881420658f2296c67c
chatsepticeye <chatprat22@gmail.com>
2026-02-01 09:44:56 +0530

    Time: 0 ms (100%), Space: 42.5 MB (8.85%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.java
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -1,6 +1,6 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        if(n%2==0) return n;
-        else return n*2;
+        // If n is even, return n. If n is odd, return n * 2.
+        return (n % 2 == 0) ? n : n * 2;
     }
 }
