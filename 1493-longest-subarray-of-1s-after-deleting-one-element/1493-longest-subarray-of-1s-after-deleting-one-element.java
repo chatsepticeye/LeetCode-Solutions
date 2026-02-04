69bb42caebe8ac73dc2ebb0809061d1d8ad4c067
chatsepticeye <chatprat22@gmail.com>
2026-02-04 20:10:10 +0530

    Time: 3 ms (86.38%), Space: 65.5 MB (42.31%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+       int ans=0;
+       int low=0;
+       int zeros=0;
+       for(int high=0;high<nums.length;high++){
+        if(nums[high]==0) zeros++;
+        while(zeros>1){
+            if(nums[low]==0) zeros--;
+            low++;
+        }
+        ans=Math.max(ans,high-low);
+       }
+       return ans;
+    }
+}
