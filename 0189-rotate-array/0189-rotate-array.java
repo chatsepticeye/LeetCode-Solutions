bb2dc151c13cae7d582f64fb89e89a33b718ea16
CHAD <chatprat22@gmail.com>
2026-02-12 18:18:14 +0530

    Time: 0 ms (100%), Space: 61.5 MB (40.49%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,18 +1,29 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-       int n=nums.length;
-       k=k%n;
-       reverse(nums,0,n-1);
-       reverse(nums,0,k-1);
-       reverse(nums,k,n-1); 
+        int n = nums.length;
+        
+        // Handle cases where k is larger than the array size
+        k = k % n; 
+        
+        // Step 1: Reverse the entire "Warehouse" (Array)
+        reverse(nums, 0, n - 1);
+        
+        // Step 2: Reverse the first k elements to fix their order
+        reverse(nums, 0, k - 1);
+        
+        // Step 3: Reverse the remaining elements to fix their order
+        reverse(nums, k, n - 1);
     }
-    public void reverse(int[]nums,int i,int j){
-        while(i<j){
-            int t=nums[i];
-            nums[i]=nums[j];
-            nums[j]=t;
-            j--;
-            i++;
+    
+    // The "Two-Pointer" helper function we mastered
+    public void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            
+            left++;
+            right--;
         }
-}
+    }
 }
