088aefd50eb0017097fd73b815a9cc110fc58112
CHAD <chatprat22@gmail.com>
2026-03-01 11:49:20 +0530

    Time: 10 ms (83.39%), Space: 46.6 MB (91.82%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,24 +1,23 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer>res=new ArrayList<>();
-        if(s.length()<p.length()) return res;//edge case
-        int [] hash=new int [128];
-        for(char ch:p.toCharArray()) hash[ch]++;
-        int c=p.length();
+        int hash[]=new int[128];
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<p.length();i++) hash[p.charAt(i)]++;
         int low=0;
+        int c=p.length();
         for(int high=0;high<s.length();high++){
-            char chigh=s.charAt(high);
-            if(hash[chigh]>0) c--;
-            hash[chigh]--;
-            if(high>=p.length()){
-                char clow=s.charAt(low);
-                if((hash[clow]>=0)) c++;
-                hash[clow]++;
+            char h=s.charAt(high);
+            if(hash[h]>0) c--;
+            hash[h]--;
+            if(c==0) ans.add(low);
+            if(high-low+1==p.length()){
+                char cl=s.charAt(low);
+                hash[cl]++;
+                if (hash[cl] > 0) c++;
                 low++;
             }
-            if(c==0) res.add(low);
-
         }
-        return res;
+        return ans;
+
     }
 }
