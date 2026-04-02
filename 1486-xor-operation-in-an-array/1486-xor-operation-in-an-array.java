67b8aefb445c73b4a0ee9052c27d7deee190bc00
CHAD <chatprat22@gmail.com>
2026-04-02 17:02:45 +0530

    Time: 0 ms (100%), Space: 42.2 MB (30.03%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int result=0;
+       for(int i=0;i<n;i++){
+        int c=start+2*i;
+         result^=c;
+         } 
+         return result;
+    }
+}
