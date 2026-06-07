869267caa6e4560c3e7320b8ff21c37dcb2d7c87
CHAD <chatprat22@gmail.com>
2026-06-07 23:57:52 +0530

    Time: 4 ms (99.35%), Space: 159.7 MB (73.18%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,31 +1,29 @@
-import java.util.ArrayList;
-
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        // 1. Create dynamic lists to hold the groups
-        ArrayList<Integer> left = new ArrayList<>();
-        ArrayList<Integer> middle = new ArrayList<>();
-        ArrayList<Integer> right = new ArrayList<>();
-        
-        // 2. Loop through the array (Fix: use < instead of <=)
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < pivot) {
-                left.add(nums[i]); // Java uses .add() instead of .append()
-            } else if (nums[i] == pivot) {
-                middle.add(nums[i]);
-            } else {
-                right.add(nums[i]);
-            }
-        } 
-        
-        // 3. Merge everything back into a primitive int[] array
         int[] result = new int[nums.length];
         int index = 0;
         
-        for (int num : left) result[index++] = num;
-        for (int num : middle) result[index++] = num;
-        for (int num : right) result[index++] = num;
+        // First pass: add all elements smaller than pivot
+        for (int num : nums) {
+            if (num < pivot) {
+                result[index++] = num;
+            }
+        }
         
-        return result;  
+        // Second pass: add all elements equal to pivot
+        for (int num : nums) {
+            if (num == pivot) {
+                result[index++] = num;
+            }
+        }
+        
+        // Third pass: add all elements greater than pivot
+        for (int num : nums) {
+            if (num > pivot) {
+                result[index++] = num;
+            }
+        }
+        
+        return result;
     }
 }
