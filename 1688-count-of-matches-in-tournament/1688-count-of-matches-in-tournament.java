53279b0a4bc49d8b64fc519bb6cd98805d0fa6a5
CHAD <chatprat22@gmail.com>
2026-06-08 20:28:12 +0530

    Time: 0 ms (100%), Space: 42.5 MB (16.96%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -1,5 +1,17 @@
 class Solution {
     public int numberOfMatches(int n) {
-        return n-1;
+        int totalMatches = 0;
+        
+        while (n > 1) {
+            if (n % 2 == 0) {
+                totalMatches += n / 2;
+                n = n / 2;         // Half the teams advance
+            } else {
+                totalMatches += (n - 1) / 2;
+                n = ((n - 1) / 2) + 1; // The remainder + 1 random team advance
+            }
+        }
+        
+        return totalMatches;
     }
 }
