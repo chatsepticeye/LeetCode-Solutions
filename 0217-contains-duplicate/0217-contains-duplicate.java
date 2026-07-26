5443936773b77ba4a0ebf18612516adf5df1aad1
CHAD <chatprat22@gmail.com>
2026-07-26 17:52:18 +0530

    Time: 17 ms (63.31%), Space: 108 MB (33.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,16 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       // Arrays.sort(nums);
-        //for(int i=0;i<nums.length-1;i++){
-          //  if(nums[i]==nums[i+1]) return true;
-         //}
-        //return false;
-    //}
-    HashSet<Integer> seen=new HashSet<>();
-    for(int num:nums){
-        if(seen.contains(num)) return true;
-        seen.add(num);
+        HashSet<Integer>seen=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(seen.contains(nums[i])){
+                return true;
+            }
+            seen.add(nums[i]);
+        }
+        return false;
     }
-    return false;
- }
 }
