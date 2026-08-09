bbafbfc121b68bd3e68afc476ae7f4d80baafe82
CHAD <chatprat22@gmail.com>
2026-08-09 15:27:29 +0530

    Time: 0 ms (100%), Space: 47.5 MB (41.12%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,13 +1,17 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int total=0;
-        for(int n:nums) total+=n;
+        int t=0;
+        for(int i=0;i<nums.length;i++){
+           t+=nums[i];
+        }
         int ls=0;
         for(int i=0;i<nums.length;i++){
-            int rs=total-ls-nums[i];
-            if(ls==rs) return i;
+            int rs=t-ls-nums[i];
+            if(ls==rs){
+                return i;
+            }
             ls+=nums[i];
         }
-        return -1;
+          return  -1;
     }
 }
