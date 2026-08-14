504663c7e7122867f93b5b696a0d16efb9f0fb64
CHAD <chatprat22@gmail.com>
2026-08-14 21:34:16 +0530

    Time: 9 ms (92.38%), Space: 47.1 MB (36.79%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,23 +1,46 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int hash[]=new int[128];
-        List<Integer> ans=new ArrayList<>();
-        for(int i=0;i<p.length();i++) hash[p.charAt(i)]++;
-        int low=0;
-        int c=p.length();
-        for(int high=0;high<s.length();high++){
-            char h=s.charAt(high);
-            if(hash[h]>0) c--;
-            hash[h]--;
-            if(c==0) ans.add(low);
-            if(high-low+1==p.length()){
-                char cl=s.charAt(low);
-                hash[cl]++;
-                if (hash[cl] > 0) c++;
-                low++;
-            }
+        // int hash[]=new int[128];
+        // List<Integer> ans=new ArrayList<>();
+        // for(int i=0;i<p.length();i++) hash[p.charAt(i)]++;
+        // int low=0;
+        // int c=p.length();
+        // for(int high=0;high<s.length();high++){
+        //     char h=s.charAt(high);
+        //     if(hash[h]>0) c--;
+        //     hash[h]--;
+        //     if(c==0) ans.add(low);
+        //     if(high-low+1==p.length()){
+        //         char cl=s.charAt(low);
+        //         hash[cl]++;
+        //         if (hash[cl] > 0) c++;
+        //         low++;
+        //     }
+        // }
+        // return ans;
+        int f1[]=new int[26];
+        int f2[]=new int[26];
+        List<Integer> res=new ArrayList<>();
+        int n=s.length();
+        int k=p.length();
+        if(k>n){ return res; }
+        for(int i=0;i<k;i++){
+            f1[s.charAt(i)-'a']++;
+            f2[p.charAt(i)-'a']++;
         }
-        return ans;
+        if(Arrays.equals(f1,f2)){
+            res.add(0);
+        }
+        for(int right=k;right<n;right++){
+            f1[s.charAt(right)-'a']++;
+            f1[s.charAt(right-k)-'a']--;
+        
+        if(Arrays.equals(f1,f2)){
+            res.add(right-k+1);
+        }
+        }
+        return res;
+        
 
     }
 }
